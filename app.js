76ad13ca5b10a0c79a4861d3f104f14e8f30171a
Quentin <quentin.le-baron@epitech.eu>
2015-10-15 15:06:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	   	var tl = new TimelineLite();
+	var tl = new TimelineLite();
     	var breadB = document.getElementById("breadB");
     	var steak = document.getElementById("steak");
     	var cheese = document.getElementById("cheese");
@@ -8,17 +8,17 @@ window.onload = function() {
     	var button = document.getElementById("toggle-button");
 
 
-		tl.to(breadB, 1, {top:"300px", ease:Bounce.easeOut}, '+=0.5');
+	tl.to(breadB, 1, {top:"300px", ease:Bounce.easeOut}, '+=0.5');
      	tl.to(steak, 2, {top:"280px", ease:Bounce.easeOut}, '-=0.5');
      	tl.to(cheese, 2, {top:"270px", ease:Bounce.easeOut}, '-=0.5');
      	tl.to(tomato, 2, {top:"255px", ease:Bounce.easeOut}, '-=0.5');
      	tl.to(breadT, 2, {top:"190px", ease:Bounce.easeOut}, '-=0.5');
 
-		tl.staggerTo("h2", 0.5, {className:"+=superShadow", top:"-=10px", ease:Power1.easeIn});
-		tl.from('h2', 2, {opacity:0}, '-=3');
+	tl.staggerTo("h2", 0.5, {className:"+=superShadow", top:"-=10px", ease:Power1.easeIn});
+	tl.from('h2', 2, {opacity:0}, '-=3');
 
 
-		button.onclick = function() {
-  			document.location.href = '404.html';
-		}
+	button.onclick = function() {
+  		document.location.href = '404.html';
+	}
 }
